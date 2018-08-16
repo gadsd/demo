1e161acfdf65edc5744d067ecfaf4a9377612107
a <a@LAPTOP-QOJ4MSB0>
2018-08-16 19:32:21 +0800

    第二次推送

--- gitdemo/src/gitdemo/hh.java
+++ gitdemo/src/gitdemo/hh.java
@@ -1,5 +1,10 @@
 package gitdemo;
 
-public class hh {
+import org.omg.Messaging.SyncScopeHelper;
 
+public class hh {
+public static void main(String[] args) {
+System.out.println("我想变得有钱");
+	
+}
 }
